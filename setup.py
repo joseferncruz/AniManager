c941c57284de7ed8e73cc5a0f6a6d6b2f673b57f
JC <29801616+joseferncruz@users.noreply.github.com>
2020-06-01 10:10:07 -0400

    initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+requirements = [
+    "numpy",
+    "pandas",
+]
+
+
+if __name__ == "__main__":
+    setup(
+        name='AniManager',
+        version='0.1.0',
+        description='quick fix for managing animals in the lab.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Jose Cruz',
+        packages=find_packages(),
+        include_package_data=True,
+        author_email='jose.cruz@nyu.edu',
+        license='GNU-3',
+        install_requires=requirements,
+    )
+
+########## More information
+# https://python-packaging-user-guide.readthedocs.io/tutorials/packaging-projects/
